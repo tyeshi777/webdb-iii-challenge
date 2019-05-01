d261a12ba45f7db159eab56ff90c2471383ea8ee
Yeshi <tyeshi777@yahoo.com>
2019-05-01 14:10:53 -0700

    almost done with cohort

--- routers/cohorts-router.js
+++ routers/cohorts-router.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const knex = require("knex");
 
 const knexConfig = {
   client: "sqlite3",
@@ -7,12 +8,68 @@ const knexConfig = {
   },
   useNullAsDefault: true
 };
-const db = knex(knexConfig());
+const db = knex(knexConfig);
 
 router.get("/", (req, res) => {
   db("cohorts")
     .then(cohort => {
       res.status(200).json(cohort);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  if (!req.body.name) {
+    res.status(400).json({ msg: "please provide a name" });
+  } else {
+    db("cohorts")
+      .insert(req.body, "id")
+      .then(ids => {
+        db("cohorts")
+          .where({ id: ids[0] })
+          .first()
+          .then(cohort => {
+            res.status(200).json(cohort);
+          })
+          .catch(err => {
+            res.status(500).json(err);
+          });
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  }
+});
+router.get("/:id", (req, res) => {
+  db("cohorts")
+    .where({ id: req.params.id })
+    .first()
+    .then(cohort => {
+      if (cohort) {
+        res.status(200).json(cohort);
+      } else {
+        res.status(404).json({ message: "no such cohorts started yet" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  db("cohorts")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({
+          message: `${count} ${count > 1 ? "cohorts" : "cohort"} updated`
+        });
+      } else {
+        res.status(400).json({ message: "no such cohort exists" });
+      }
     })
     .catch(err => {
       res.status(500).json(err);
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const helmet = require("helmet");
+const cohortsRouter = require("./routers/cohorts-router.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
+server.use("/api/cohorts", cohortsRouter);
 module.exports = server;
